601f481a65cd2ce3fad6361bbaa28b002708b141
hxwang <cshuangxinwang@gmail.com>
2016-04-08 17:06:19 -0400

    updatE

--- src/mem.cc
+++ src/mem.cc
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <iostream>
 #include <string.h>
+#include <ctime>
 using namespace std;
 
 void print_usage(char** argv) {
@@ -19,19 +20,28 @@ int main(int argc, char** argv) {
 		return 0;
 	} else {
 		size = std::stoi(string(argv[1]));
-	}	
+	}
+
+	auto start = clock();
 
 	cout<<"allocating mem for "	<<size<<" MB"<<endl;
 
 	long long length = (long long)size*1024*1024/sizeof(int);
+	
 
 	int* buf = new int[length];
+	
+	cout<<"memory allocated in "<< (clock()-start)*1.0 / CLOCKS_PER_SEC << " s"<<endl;
+	
+	start = clock();
 
 	cout<<"filling memory"<<endl;
 
 	for(long long i=0;i<length;++i) {
 		buf[i] = i;
 	}
+	
+	cout<<"memory filled in "<< (clock()-start)*1.0 / CLOCKS_PER_SEC << " s"<<endl;
 
 	if(buf==0) {
 		cout<<"Error! failed to allocate memory!"<<endl;
