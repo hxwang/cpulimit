b5ce6f6528f5b4f883386d6af1144b61eeb101ed
hwang14 <hwang14@gmu.edu>
2016-05-15 16:09:50 -0400

    update

--- src/copy_to_vm.sh
+++ src/copy_to_vm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ssh-keygen -f "/home/hwang14/.ssh/known_hosts" -R 10.0.47.2
-scp -i ../../../vm-key/cloud.key /usr/bin/stress cpulimit mem ubuntu@10.0.47.2:~/
+scp -i ../../openstack/setup/key/poweredge.key /usr/bin/stress cpulimit mem ubuntu@10.0.47.2:~/
